e513e18a478e5749b05aa53d7eb267f9e2a47866
Artur Buja <artur.buja2@gmail.com>
2023-09-06 19:38:49 +0200

    global styling tbc..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
-import styles from "./homepage.module.css";
+import Link from 'next/link';
+import styles from './homepage.module.css';
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Link href='/'>
+        <h1>Home</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus at,
+        facere fugit perspiciatis quae nulla? Iste quis maiores, ullam veritatis
+        suscipit vel adipisci similique assumenda rem minus ex explicabo.
+        Nostrum voluptates repudiandae voluptatibus sed doloremque ad soluta
+        suscipit. Reprehenderit, laboriosam quibusdam. Debitis, excepturi velit
+        odio iure alias officia tenetur deleniti in veritatis, labore maiores.
+        Nihil, ab repellendus rem, quod minima cupiditate necessitatibus
+        delectus neque itaque veritatis at repellat tempora, dolorem odio in.
+        Totam voluptate aperiam ipsum, velit ea voluptates non suscipit, eveniet
+        natus harum quisquam dolorum quas. Veritatis tenetur dicta quibusdam
+        officiis deserunt soluta reiciendis error, ad veniam minima enim dolores
+        libero unde praesentium magnam, architecto aut ipsum ipsa blanditiis
+        rerum porro odit omnis. Nostrum, ipsum iure. Maxime nostrum perferendis
+        debitis, amet blanditiis facilis vel cum tempora iusto explicabo sunt
+        eligendi accusantium deserunt optio cumque atque, quae ad esse veniam
+        illo! Voluptate, ea at ad tenetur et suscipit nesciunt libero cum nam
+        cupiditate! Accusamus repudiandae rerum optio aperiam quos, cupiditate
+        sapiente libero ipsum adipisci placeat dolores culpa architecto nulla
+        quasi, atque, nemo odit saepe deleniti incidunt! Repudiandae cupiditate
+        asperiores alias quisquam velit at praesentium eos enim autem!
+        Dignissimos odit velit sunt ipsam dolores labore? Id placeat ducimus ad
+        doloremque dolores fugit dignissimos est, corporis at, dicta ut
+        praesentium reprehenderit inventore recusandae voluptates perferendis
+        odit, voluptatibus nostrum possimus pariatur unde soluta debitis nihil!
+        Harum officiis nostrum quos mollitia provident soluta cumque odio saepe
+        placeat, perspiciatis voluptatibus labore explicabo! Consequatur harum
+        odit nulla magni vero eius? Explicabo nulla aliquam culpa corrupti porro
+        eaque tempore amet exercitationem. Nobis nihil officia iusto libero
+        architecto cum ullam pariatur possimus atque consequuntur eligendi, sunt
+        dolores earum facilis corrupti cumque amet explicabo. Cumque beatae,
+        odio quod ullam fugit voluptate neque consectetur nisi eum rerum modi
+        quis rem, libero nesciunt voluptatem molestiae corporis accusamus
+        tempore quos provident. Cum?
+      </Link>
+    </div>
+  );
 }
