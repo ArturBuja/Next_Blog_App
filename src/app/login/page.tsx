a22daf24993e81351cd7bcb35452098072e9a97e
Artur Buja <artur.buja2@gmail.com>
2023-09-12 13:49:49 +0200

    finish write section

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,15 @@
 import styles from './loginPage.module.css';
 
-const page = () => {
-  return <div>page</div>;
+const LoginPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.socialButton}>Sing in with Google</div>
+        <div className={styles.socialButton}>Sing in with Github</div>
+        <div className={styles.socialButton}>Sing in with Facebook</div>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default LoginPage;
