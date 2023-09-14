d727d00b545e108909c837d31119583b88868e1e
Artur Buja <artur.buja2@gmail.com>
2023-09-14 22:00:34 +0200

    remove link

--- src/utils/conenct.ts
+++ src/utils/conenct.ts
@@ -15,5 +15,3 @@ const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 export default prisma;
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-// https://www.youtube.com/watch?v=DpYE5zPDRVQ&list=WL&index=5
