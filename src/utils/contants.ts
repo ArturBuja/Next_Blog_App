f34fadc444ac3d9586516a7c6a72d54cd0b575ca
Artur Buja <artur.buja2@gmail.com>
2023-09-17 03:01:54 +0200

    update link

--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -1,3 +1,3 @@
 export const POST_PER_PAGE = 4;
-export const API_URL = 'fromthelinetolife.vercel.app/api';
+export const API_URL = 'fromlinestolife.vercel.app/api';
 export const API_URL_TEST = `${process.env.NEXT_PUBLIC_BASE_URL}/api`;
