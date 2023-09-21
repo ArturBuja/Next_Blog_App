f978213cfa5deb20d4afe42e434a3e7eee26dc6a
Artur Buja <artur.buja2@gmail.com>
2023-09-21 12:37:33 +0200

    changed utl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,11 +13,14 @@ import { Session } from 'next-auth';
 const inter = Inter({ subsets: ['latin'] });
 
 async function getSession(cookie: string): Promise<Session> {
-  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/auth/session`, {
-    headers: {
-      cookie,
-    },
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/session`,
+    {
+      headers: {
+        cookie,
+      },
+    }
+  );
   const session = await response.json();
 
   return Object.keys(session).length > 0 ? session : null;
