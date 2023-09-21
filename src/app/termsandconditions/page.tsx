79b5c271a46b5258e07c266cef630538e391c2b9
Artur Buja <artur.buja2@gmail.com>
2023-09-21 20:21:38 +0200

    added terms and privacy

--- src/app/termsandconditions/page.tsx
+++ src/app/termsandconditions/page.tsx
@@ -161,7 +161,7 @@ const Privacy = () => {
         of the Company and any of its suppliers under any provision of this
         Terms and Your exclusive remedy for all of the foregoing shall be
         limited to the amount actually paid by You through the Service or 100
-        USD if You haven't purchased anything through the Service.
+        USD if You haven&rsquo;t purchased anything through the Service.
       </p>
       <p>
         To the maximum extent permitted by applicable law, in no event shall the
@@ -180,8 +180,8 @@ const Privacy = () => {
         Some states do not allow the exclusion of implied warranties or
         limitation of liability for incidental or consequential damages, which
         means that some of the above limitations may not apply. In these states,
-        each party's liability will be limited to the greatest extent permitted
-        by law.
+        each party&rsquo;s liability will be limited to the greatest extent
+        permitted by law.
       </p>
       <h2>&quot;AS IS&quot; and &quot;AS AVAILABLE&quot; Disclaimer</h2>
       <p>
@@ -205,8 +205,8 @@ const Privacy = () => {
       </p>
       <p>
         Without limiting the foregoing, neither the Company nor any of the
-        company's provider makes any representation or warranty of any kind,
-        express or implied: (i) as to the operation or availability of the
+        company&rsquo;s provider makes any representation or warranty of any
+        kind, express or implied: (i) as to the operation or availability of the
         Service, or the information, content, and materials or products included
         thereon; (ii) that the Service will be uninterrupted or error-free;
         (iii) as to the accuracy, reliability, or currency of any information or
@@ -262,9 +262,9 @@ const Privacy = () => {
       <p>
         Except as provided herein, the failure to exercise a right or to require
         performance of an obligation under these Terms shall not effect a
-        party's ability to exercise such right or require such performance at
-        any time thereafter nor shall the waiver of a breach constitute a waiver
-        of any subsequent breach.
+        party&rsquo;s ability to exercise such right or require such performance
+        at any time thereafter nor shall the waiver of a breach constitute a
+        waiver of any subsequent breach.
       </p>
       <h2>Translation Interpretation</h2>
       <p>
@@ -276,7 +276,7 @@ const Privacy = () => {
       <p>
         We reserve the right, at Our sole discretion, to modify or replace these
         Terms at any time. If a revision is material We will make reasonable
-        efforts to provide at least 30 days' notice prior to any new terms
+        efforts to provide at least 30 days&rsquo; notice prior to any new terms
         taking effect. What constitutes a material change will be determined at
         Our sole discretion.
       </p>
